3fa0f679a85040d133ab0b2070850069e2fc0f3a
philipzhou2009 <philip.zhou.2009@gmail.com>
2013-08-05 17:33:38 +0800

    miscinfo: this new module is for misc info input, for example, general contractor;

---
+++ HCS/application/modules/miscinfo/controllers/ManageController.php
@@ -0,0 +1,27 @@
+<?php
+
+class Miscinfo_ManageController extends Zend_Controller_Action
+{
+    private $_em;
+    private $_material;
+
+    public function init()
+    {
+        $this->_em = Zend_Registry::get('em');
+        $this->_material = $this->_em->getRepository('Synrgic\Infox\Material');
+        $this->_supplier = $this->_em->getRepository('Synrgic\Infox\Supplier');
+        $this->_application = $this->_em->getRepository('Synrgic\Infox\Application');
+        $this->_matappdata = $this->_em->getRepository('Synrgic\Infox\Matappdata');
+        $this->_supplyprice = $this->_em->getRepository('Synrgic\Infox\Supplyprice');
+        $this->_user = $this->_em->getRepository('Synrgic\User');
+        $this->_materialtype = $this->_em->getRepository('Synrgic\Infox\Materialtype');
+        $this->_site = $this->_em->getRepository('Synrgic\Infox\Site');
+        $this->_humanresource = $this->_em->getRepository('Synrgic\Infox\Humanresource');
+    }
+
+    public function indexAction()
+    {
+    
+    }    
+
+}
