a4cb57012caad422814f88afc5764107a3c3a99c
philipzhou2009 <philip.zhou.2009@gmail.com>
2013-08-08 11:56:52 +0800

    miscinfo: fully work, support site property and contractor;

--- HCS/application/modules/miscinfo/controllers/ManageController.php
+++ HCS/application/modules/miscinfo/controllers/ManageController.php
@@ -6,34 +6,20 @@ class Miscinfo_ManageController extends Zend_Controller_Action
 
     public function init()
     {
-        $this->_em = Zend_Registry::get('em');
-        $this->_material = $this->_em->getRepository('Synrgic\Infox\Material');
-        $this->_supplier = $this->_em->getRepository('Synrgic\Infox\Supplier');
-        $this->_application = $this->_em->getRepository('Synrgic\Infox\Application');
-        $this->_matappdata = $this->_em->getRepository('Synrgic\Infox\Matappdata');
-        $this->_supplyprice = $this->_em->getRepository('Synrgic\Infox\Supplyprice');
-        $this->_user = $this->_em->getRepository('Synrgic\User');
-        $this->_materialtype = $this->_em->getRepository('Synrgic\Infox\Materialtype');
-        $this->_site = $this->_em->getRepository('Synrgic\Infox\Site');
-        $this->_humanresource = $this->_em->getRepository('Synrgic\Infox\Humanresource');
 
+        $this->_em = Zend_Registry::get('em');
         $this->_miscinfo = $this->_em->getRepository('Synrgic\Infox\Miscinfo');
     }
 
     public function indexAction()
     {
         $infos = $this->_miscinfo->findAll();
-        $this->view->infos = $infos;
-        
-        // general contractor                
-        $info01 = $this->_miscinfo->findOneBy(array("label"=>"info01"));
-        $this->view->info01 = $info01;
+        $this->view->infos = $infos;        
     }    
 
     public function addinfoAction()
     {
-        $this->turnoffview();
-        
+        $this->turnoffview();        
         if(0)
         {
             $requests = $this->getRequest()->getPost();
---
+++ HCS/bin/data/infox_miscinfo.php
@@ -0,0 +1,32 @@
+<?
+/**
+ * miscinfo
+ * kinds of simple information, for staff editing
+ * these data is important for modules like project/site
+ */
+
+$tuples = array( 
+/* id, label, namechs, nameeng, values */
+array(1, "info01", "总承包单位", "General Contractor", ""),
+array(2, "info02", "工程性质", "Site Property", ""),
+);
+
+foreach( $tuples as $tuple ){
+    $data = new \Synrgic\Infox\Miscinfo();
+
+    // http://stackoverflow.com/questions/5301285/explicitly-set-id-with-doctrine-when-using-auto-strategy
+    // put it here to reset id generator
+    //$metadata = $em->getClassMetaData(get_class($data));
+    //$metadata->setIdGenerator(new \Doctrine\ORM\Id\AssignedGenerator());
+    //$data->setId($tuple[0]);
+
+    $data->setLabel($tuple[1]);
+    $data->setNamechs($tuple[2]);
+    $data->setNameeng($tuple[3]);
+    $data->setValues($tuple[4]);
+
+    $em->persist($data);
+}
+
+$em->flush();
+?>
