3239a387e082679d2bfe9861a98f0f6ad9c47e2d
philipzhou2009 <philip.zhou.2009@gmail.com>
2013-08-14 17:55:06 +0800

    workeronsite: update UI tables;

--- HCS/application/modules/worker/views/helpers/Worker.php
+++ HCS/application/modules/worker/views/helpers/Worker.php
@@ -20,19 +20,19 @@ class GridHelper_Worker extends Grid_Helper_Abstract
 
     protected function td_site($field, $row) 
     {
-        /*
         $em = Zend_Registry::get('em');
         $wc = $em->getRepository('Synrgic\Infox\Workercompanyinfo');  
         $wcobj = $wc->findOneBy(array("id"=>$row['id']));                
         $siteobj = $wcobj ? $wcobj->getSite() : null;
         $sitename = $siteobj ? $siteobj->getName() : "&nbsp;";        
         return $sitename;
-        */
 
+        /*
         $site = $row[$field];
         $sitename = $site ? $site->getName() : "&nbsp;";
 
         return $sitename;        
+        */    
     }
 
     protected function td_securityexp($field, $row) 
@@ -44,28 +44,7 @@ class GridHelper_Worker extends Grid_Helper_Abstract
         $date = $sedate ? $sedate->format('Y-m-d') : "&nbsp;";
 
         return $date;
-    }
-
-    protected function td_begindate($field, $row) 
-    {
-        if($row[$field])
-        {
-            return $row[$field]->format("Y/m/d");
-        }
-        
-        return "&nbsp;";
-    }    
-
-    protected function td_enddate($field, $row) 
-    {
-        if($row[$field])
-        {
-            return $row[$field]->format("Y/m/d");
-        }
-        
-        return "&nbsp;";
-    }    
-
+    }  
 }
 
 ?>
