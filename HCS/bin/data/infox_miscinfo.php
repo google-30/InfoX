10cc2779870ad510851925801be1ebe5eceaa875
philipzhou2009 <philip.zhou.2009@gmail.com>
2013-08-19 17:49:14 +0800

    miscinfo: add default values

--- HCS/bin/data/infox_miscinfo.php
+++ HCS/bin/data/infox_miscinfo.php
@@ -8,7 +8,7 @@
 $tuples = array( 
 /* id, label, namechs, nameeng, values */
 array(1, "info01", "总承包单位", "General Contractor", ""),
-array(2, "info02", "工程性质", "Site Property", ""),
+array(2, "info02", "工程性质", "Site Property", "公寓;组屋;学校;幼儿园;"),
 array(3, "info03", "工人考勤", "Worker Attendance", "病假;有薪假;无薪假;出差;其他事由;"),
 array(4, "info04", "工种", "Worker Type", "木工;电工;油漆工;瓦工;"),
 array(5, "info05", "设备状态", "Emachinery Status", "正常;遗失;修理;损坏;报废"),
