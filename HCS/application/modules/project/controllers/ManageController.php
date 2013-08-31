6fc186f6c592bd2cdf17b6c1a678703d9d52df58
philipzhou2009 <philip.zhou.2009@gmail.com>
2013-08-31 10:21:44 +0800

    site:remove workers in edit function;

--- HCS/application/modules/project/controllers/ManageController.php
+++ HCS/application/modules/project/controllers/ManageController.php
@@ -48,22 +48,6 @@ class Project_ManageController extends Zend_Controller_Action
         //echo "id=$id<br>";
         $maindata = $this->_site->findOneBy(array("id"=>$id));
         $this->view->maindata = $maindata;
-        /*    
-        $query = $this->_em->createQuery(
-        'select w, wc.companylabel, wc.hwage, ws.worktype, ws.worklevel,
-        (select site.name from Synrgic\Infox\Site site where site.id = wc.site) as sitename
-        from Synrgic\Infox\Worker w LEFT JOIN w.workercompanyinfo wc LEFT JOIN w.workerskill ws 
-        where wc.site = ' . $id
-         );
-        */    
-        $query = $this->_em->createQuery(
-        'select w.nameeng, w.namechs, w.fin, wc.companylabel, wc.hwage, ws.worktype, ws.worklevel,
-        (select site.name from Synrgic\Infox\Site site where site.id = wc.site) as sitename
-        from Synrgic\Infox\Worker w LEFT JOIN w.workercompanyinfo wc LEFT JOIN w.workerskill ws 
-        where wc.site = ' . $id
-         );
-        $result = $query->getResult();
-        $this->view->workers = $result;                
     } 
 
     public function deleteAction()
@@ -281,17 +265,6 @@ class Project_ManageController extends Zend_Controller_Action
 
         $records = $this->_workeronsite->findBy(array("site"=>$siteobj));
         $this->view->records = $records;
-
-        /*
-        $query = $this->_em->createQuery(
-        'select w.nameeng, w.namechs, w.fin, wc.companylabel, wc.hwage, ws.worktype, ws.worklevel,
-        (select site.name from Synrgic\Infox\Site site where site.id = wc.site) as sitename
-        from Synrgic\Infox\Worker w LEFT JOIN w.workercompanyinfo wc LEFT JOIN w.workerskill ws 
-        where wc.site = ' . $id
-         );
-        $result = $query->getResult();
-        $this->view->workers = $result;
-        */        
     }
 
     public function emachineryAction()
