6088ee042db28dfc1d02d5b77223e0d8e6f8e6d1
philipzhou2009 <philip.zhou.2009@gmail.com>
2013-09-14 00:01:41 +0800

    UI: improve UI elements;

--- HCS/application/modules/management/controllers/DashboardController.php
+++ HCS/application/modules/management/controllers/DashboardController.php
@@ -62,6 +62,7 @@ class Management_DashboardController extends Zend_Controller_Action
         $icon = $page->icon;
         if(!isset($icon)) {
             $page->icon = '/images/lightsoff.png';
+            $page->iconhover = '/images/lightson.png';
         }
 
 	/*
