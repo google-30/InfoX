061a17f69cb2f5860d381c3e35e4859c1ed2d52b
philipzhou2009 <philip.zhou.2009@gmail.com>
2013-09-22 12:36:08 +0800

    site_attendance: implementing index page;

--- HCS/application/modules/project/controllers/AttendanceController.php
+++ HCS/application/modules/project/controllers/AttendanceController.php
@@ -1,5 +1,6 @@
 <?php
 include 'InfoX/infox_project.php';
+include 'InfoX/infox_user.php';
 
 class Project_AttendanceController extends Zend_Controller_Action
 {
@@ -12,8 +13,101 @@ class Project_AttendanceController extends Zend_Controller_Action
 
     public function indexAction()
     {
+        //$username = infox_user::getUserName();
+        //echo $username . "<br>";
+        //$userRole = infox_user::getUserRole();
+        //echo $userRole . "<br>";
+    
+        $sites = infox_user::getUserSites();
+        $siteid = $this->getParam("siteid", 0);
         
+        // get construction time 
+        $siteobj = $this->_site->findOneBy(array("id"=>$siteid));        
+        $startdate = $siteobj->getStart();
+        $stopdate = $siteobj->getStop();
+        $months = $this->getMonths($startdate, $stopdate);
+
+        $this->view->sites = $sites;
+        $this->view->siteid = $siteid;
+        $this->view->workingmonths=$months;
     }
 
+    private function getMonths($start, $stop)
+    {
+        $startyear = $start->format("Y");
+        $stopyear = $stop->format("Y");
+
+        $deltayear = $stopyear - $startyear;
+        //echo "deltayear=$deltayear<br>";
+
+        $startmonth = $start->format("m");
+        //echo "startmonth=$startmonth<br>";        
+        $stopmonth = $stop->format("m");
+        //echo "stopmonth=$stopmonth<br>";
+        
+        $dateArr = array();
+
+        if($deltayear==0)
+        {// same year 
+            $deltamonth = $stopmonth - $startmonth;
+            //echo "deltamonth=$deltamonth<br>";
+            for($i=0; $i<$deltamonth; $i++)
+            {
+                $year = $startyear;    
+                $month = $startmonth + $i;
+                $date = new Datetime("$year-$month");
+                $dateArr[] = $date;
+            }
+        }
+        else
+        {
+            // first year
+            $deltamonth = 12 - $startmonth +1;
+            $i = 0;
+            for($i=0; $i<$deltamonth; $i++)
+            {
+                $year = $startyear;    
+                $month = $startmonth + $i;
+                $date = new Datetime("$year-$month");
+                $dateArr[] = $date;
+            }
+
+            // years
+            for($i=0; $i<$deltayear-1; $i++)
+            {
+                $year = $startyear + $i + 1;
+                for($j=0; $j<12; $j++)
+                {
+                    $month = $j+1;
+                    $date = new Datetime("$year-$month");
+                    $dateArr[] = $date;
+                }
+            }
+
+            // last year
+            $deltamonth = $stopmonth;
+            for($i=0; $i<$deltamonth; $i++)
+            {
+                $month = $i+1;
+                $year = $stopyear;
+                $date = new Datetime("$year-$month");
+                $dateArr[] = $date;
+
+            }            
+        }
+
+        /*
+        foreach($dateArr as $tmp)
+        {
+            echo $tmp->format("Y-m") . "<br>";
+        }
+        */
+
+        return $dateArr;
+    }
+
+    public function submitAction()
+    {
+    }
 
 }
---
+++ HCS/library/InfoX/infox_user.php
@@ -0,0 +1,78 @@
+<?php
+
+class infox_user
+{
+    //private static $_em = Zend_Registry::get('em');
+    private $_ctl;
+
+    private function __construct() { }
+    /*
+    private function __construct($controller) 
+    {
+        $_ctl = $controller; 
+    }
+    */
+
+    public static function getUserName()
+    {
+        $auth = Zend_Auth::getInstance();
+        if ($auth->hasIdentity()) {
+            $username = $auth->getIdentity()->username;
+            //echo "username=$username<br>";            
+            return $username;
+        }
+        else
+        {
+            $ted = "Ted...";
+            //echo "username=$ted<br>";            
+            return $ted;
+        }             
+    } 
+
+    public static function getUserRole()
+    {
+        $username = self::getUserName();
+        $em = Zend_Registry::get('em');        
+        $userRepo = $em->getRepository('Synrgic\User');
+        $userObj = $userRepo->findOneBy(array("username"=>$username));
+        $role = $userObj ? $userObj->getRole() : "";
+        //echo "XXXXXXXXXX=" . $role;
+        return $role;
+    }
+
+    public static function getUserSites()
+    {
+        $em = Zend_Registry::get('em');        
+        $humanresRepo = $em->getRepository('Synrgic\Infox\Humanresource');
+        $siteRepo = $em->getRepository('Synrgic\Infox\Site');
+
+        $username = self::getUserName();
+        $user = $humanresRepo->findOneBy(array("username"=>$username));
+
+        $role = self::getUserRole();
+        if($role == "leader")
+        {
+            $id = $user->getId();
+
+            $sites1 = $this->_site->findBy(array("permission1"=>true));
+            $sites = array();
+            foreach($sites1 as $tmp)
+            {
+                $leaders = $tmp->getLeaders();
+                $array = explode(";", $leaders);
+                if(in_array($id, $array))
+                {
+                    $sites[] = $tmp;
+                }
+            }
+        }
+        else
+        {
+            $sites = $siteRepo->findAll();
+        }
+
+        //$this->view->sites = $sites;
+        return $sites;
+    }
+
+}
