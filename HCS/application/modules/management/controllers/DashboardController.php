9c2f0c380b838167ca801f8310587fa300f5d052
philipzhou2009 <philip.zhou.2009@gmail.com>
2013-12-28 19:12:46 +0800

    UI: improve some UI elements;

--- HCS/application/modules/management/controllers/DashboardController.php
+++ HCS/application/modules/management/controllers/DashboardController.php
@@ -1,55 +1,52 @@
 <?php
-/*-
- * Copyright (c) 2012 Synrgic Research Pte Ltd
-* All rights reserved
-*
-* Redistribution of this file in source code, or binary form is
-* expressly not permitted without prior written approval of
-* Synrgic Research Pte Ltd
-*/
 
-class Management_DashboardController extends Zend_Controller_Action
-{
+/* -
+ * Copyright (c) 2012 Synrgic Research Pte Ltd
+ * All rights reserved
+ *
+ * Redistribution of this file in source code, or binary form is
+ * expressly not permitted without prior written approval of
+ * Synrgic Research Pte Ltd
+ */
+
+class Management_DashboardController extends Zend_Controller_Action {
+
     private $em = null;
     private $user;
     private $acl;
 
-    public function init()
-    {
+    public function init() {
         $this->em = Zend_Registry::get('em');
         $this->_helper->layout()->setLayout('dashboard');
-	$this->acl =Zend_Registry::get('acl');
-	$auth=Zend_Auth::getInstance();
-	$this->user = $auth->getIdentity();
+        $this->acl = Zend_Registry::get('acl');
+        $auth = Zend_Auth::getInstance();
+        $this->user = $auth->getIdentity();
     }
 
-    public function indexAction()
-    {
+    public function indexAction() {
         $this->view->dashboard = $this->inflateMenuTree();
     }
-    
+
     private function inflateMenuTree() {
         $navigation = Zend_Registry::get('navigation');
         $dashboard = array();
-        $dashboard[] = array('label' => $this->view->translate(''), 'pages'=>null);
+        $dashboard[] = array('label' => $this->view->translate(''), 'pages' => null);
         $appgroup = array();
-        foreach($navigation->getChildren() as $page) {
-            if($page->count()>0 ) {
+        foreach ($navigation->getChildren() as $page) {
+            if ($page->count() > 0) {
                 //printf("%s<br>",$page->Label);
                 $pages = array();
                 $this->groupPages($page, $pages);
-                if(empty($pages)) {
+                if (empty($pages)) {
                     //no children, put it to its parent
                     $this->addPageToGroup($page, $appgroup);
+                } else {
+                    $dashboard[] = array('label' =>
+                        $this->view->translate($page->Label), 'pages' => $pages);
                 }
-                else {
-                    $dashboard[] = array('label'=>
-					 $this->view->translate($page->Label), 'pages'=>$pages);
-                }    
-            }
-            else {
+            } else {
                 //printf("xxx %s\<br>", $page->Label);
-                if($page->Label !== 'Dashboard' && $page->isVisible()) {
+                if ($page->Label !== 'Dashboard' && $page->isVisible()) {
                     $this->addPageToGroup($page, $appgroup);
                 }
             }
@@ -57,45 +54,45 @@ class Management_DashboardController extends Zend_Controller_Action
         $dashboard[0]['pages'] = $appgroup;
         return $dashboard;
     }
-    
+
     private function addPageToGroup($page, &$group) {
         $icon = $page->icon;
-        if(!isset($icon)) {
+        if (!isset($icon)) {
             $page->icon = '/images/lightsoff.png';
             $page->iconhover = '/images/lightson.png';
         }
 
-	/*
-	 * ACL Debugging. Enable the below to view all notes with postfix
-	 * comment -benjsc 20130212
-	if($page->getResource())
-	    $show = ($this->acl->isAllowed($this->user->role, $page->getResource(),$page->getPrivilege())==1)?"Y":"N";
-	else 
-	    $show="U";
-	 */
+        /*
+         * ACL Debugging. Enable the below to view all notes with postfix
+         * comment -benjsc 20130212
+          if($page->getResource())
+          $show = ($this->acl->isAllowed($this->user->role, $page->getResource(),$page->getPrivilege())==1)?"Y":"N";
+          else
+          $show="U";
+         */
 
-	if( ($page->getResource() && 
-	     	$this->acl->isAllowed($this->user->role,$page->getResource(),$page->getPrivilege())) || 
-	     isset($show)){
+        if (($page->getResource() &&
+                $this->acl->isAllowed($this->user->role, $page->getResource(), $page->getPrivilege())) ||
+                isset($show)) {
 
-	    $page->Label = $this->view->translate($page->Label);
-	    if(isset($show))
-		$page->Label.=$show;
+            $page->Label = $this->view->translate($page->Label);
+            if (isset($show))
+                $page->Label.=$show;
 
-	    $group[] = $page;
-	}
+            $group[] = $page;
+        }
     }
-    
+
     private function groupPages($container, &$group) {
-        foreach($container as $page) {
-            if($page->hasChildren()) {
+        foreach ($container as $page) {
+            if ($page->hasChildren()) {
                 $this->groupPages($page, $group);
-            }
-            else {
-                if($page->isVisible()) {
+            } else {
+                if ($page->isVisible()) {
                     $this->addPageToGroup($page, $group);
                 }
             }
         }
     }
+
 }
